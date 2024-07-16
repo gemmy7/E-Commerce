40624c70df4cdb807206baf2818c025fe218c8f7
DESKTOP-4B5QCK0\oohhp <oohhpp9080@gmail.com>
2024-07-16 15:42:56 +0300

    onboarding Screen with page indicator responsive_helper preparing for signin screen

--- lib/core/themes/app_colors.dart
+++ lib/core/themes/app_colors.dart
@@ -4,4 +4,6 @@ class AppColors {
   static const primary = Color(0xff8E6CEF);
   static const background = Color(0xff1D182A);
   static const secondBackground = Color(0xff342F3F);
+  static const Color gray = Color(0xFF757575);
+  static const Color lightGray = Color(0xFFC2C2C2);
 }
---
+++ lib/core/widgets/app_text_button.dart
@@ -0,0 +1,56 @@
+import 'package:e_commerce/core/themes/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class TextButtonWidget extends StatelessWidget {
+  final double? borderRadius;
+  final Color? backgroundColor;
+  final double? horizontalPadding;
+  final double? verticalPadding;
+  final double? buttonWidth;
+  final double? buttonHeight;
+  final String buttonText;
+  final TextStyle textStyle;
+  final VoidCallback onPressed;
+  const TextButtonWidget({
+    super.key,
+    this.borderRadius,
+    this.backgroundColor,
+    this.horizontalPadding,
+    this.verticalPadding,
+    this.buttonHeight,
+    this.buttonWidth,
+    required this.buttonText,
+    required this.textStyle,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius ?? 16.0),
+          ),
+        ),
+        backgroundColor: WidgetStatePropertyAll(
+          backgroundColor ?? AppColors.primary,
+        ),
+        padding: WidgetStateProperty.all<EdgeInsets>(
+          EdgeInsets.symmetric(
+            horizontal: horizontalPadding ?? 12,
+            vertical: verticalPadding ?? 14,
+          ),
+        ),
+        fixedSize: WidgetStateProperty.all(
+          Size(buttonWidth ?? double.maxFinite, buttonHeight ?? 50),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        buttonText,
+        style: textStyle,
+      ),
+    );
+  }
+}
